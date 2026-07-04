7c534786d180d9b3727ca29e474c3dd2aa4b668a
Philip <philipgricius@gmail.com>
2026-07-04 14:46:24 +0200

    basic lookup

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::fs::File;
 
 use polars::{
+    df,
     error::PolarsResult,
     io::{SerWriter, csv::write::CsvWriter},
     lazy::{
-        dsl::{col, lit},
-        frame::{LazyCsvReader, LazyFileListReader},
+        dsl::{col, lit, when},
+        frame::{IntoLazy, LazyCsvReader, LazyFileListReader},
     },
 };
 
@@ -14,20 +15,39 @@ fn main() -> PolarsResult<()> {
         .with_has_header(true)
         .finish()?;
 
-    let processed_lf = lf
-        .filter(col("Money In").gt(lit(100)))
-        .with_column((col("Money In") * lit(1.1)).alias("testt"))
-        .select([col("Nr"), col("Posting Date"), col("Money In"), col("testt")]);
+    let original_categories = vec!["Food", "Communication"];
+    let new_categories = vec!["Foooooood", "Comms"];
 
-    let mut df = processed_lf.collect()?;
+    let lookup = df![
+        "Original Category" => original_categories,
+        "New Category" => new_categories,
+    ]?
+    .lazy();
+
+    let joined = lf
+        .left_join(lookup, col("Parent Category"), col("Original Category"))
+        .select([col("Nr"), col("Parent Category"), col("New Category")]);
+
+    let df = joined.collect()?;
+
     println!("{}", df);
 
-    let mut file = File::create("new.csv").expect("Failed to create the file");
+    // let processed_lf = lf
+    //     .filter(col("Money In").gt(lit(100)))
+    //     .with_column((col("Money In") * lit(1.1)).alias("testt"))
+    //     .select([
+    //         col("Nr"),
+    //         col("Posting Date"),
+    //         col("Money In"),
+    //         col("testt"),
+    //     ]);
 
-    CsvWriter::new(&mut file)
-        .include_header(true)
-        .with_separator(b',')
-        .finish(&mut df)?;
+    // let mut file = File::create("new.csv").expect("Failed to create the file");
+
+    // CsvWriter::new(&mut file)
+    //     .include_header(true)
+    //     .with_separator(b',')
+    //     .finish(&mut df)?;
 
     Ok(())
 }
