ef62006d27c8d25914f2beaf690da24438d1ffec
Philip <philipgricius@gmail.com>
2026-07-05 15:41:29 +0200

    trim columns and headers

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::{
 };
 
 use polars::{
+    datatypes::DataType,
     df,
     error::PolarsResult,
     io::{
@@ -13,7 +14,7 @@ use polars::{
         csv::{read::CsvReader, write::CsvWriter},
     },
     lazy::{
-        dsl::{Expr, col, lit, when},
+        dsl::{Expr, col, dtype_col, lit, when},
         frame::{IntoLazy, LazyCsvReader, LazyFileListReader},
     },
     prelude::NULL,
@@ -45,13 +46,23 @@ fn main() -> PolarsResult<()> {
         .expect("Failed to extract file name");
 
     println!("Opening user profile ...");
-    let lookup = LazyCsvReader::new(format!("{}_lookup.csv", username))
+    let mut lookup = LazyCsvReader::new(format!("{}_lookup.csv", username))
         .with_has_header(true)
         .finish()?
-        .collect()?;
+        .with_columns([dtype_col(&DataType::String).str().strip_chars(lit(""))]);
+    let schema = lookup.collect_schema()?;
+    let old_names = schema
+        .iter_names()
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>();
+    let new_names = old_names
+        .iter()
+        .map(|s| s.trim().to_string())
+        .collect::<Vec<String>>();
+    let new_lookup = lookup.rename(&old_names, &new_names, false).collect()?;
 
-    let masks = lookup.column("Mask")?.str()?;
-    let categories = lookup.column("New Category")?.str()?;
+    let masks = new_lookup.column("Mask")?.str()?;
+    let categories = new_lookup.column("New Category")?.str()?;
     let mut expr = lit(NULL);
 
     for (opt_mask, opt_category) in masks.into_iter().zip(categories.into_iter()) {
@@ -63,12 +74,23 @@ fn main() -> PolarsResult<()> {
     }
 
     println!("Opening {} ...", bank_statement_file_name);
-    let data = LazyCsvReader::new(bank_statement_path)
+    let mut data = LazyCsvReader::new(bank_statement_path)
         .with_has_header(true)
-        .finish()?;
+        .finish()?
+        .with_columns([dtype_col(&DataType::String).str().strip_chars(lit(""))]);
+    let schema = data.collect_schema()?;
+    let old_names = schema
+        .iter_names()
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>();
+    let new_names = old_names
+        .iter()
+        .map(|s| s.trim().to_string())
+        .collect::<Vec<String>>();
+    let new_data = data.rename(&old_names, &new_names, false);
 
     println!("Filtering {} ...", bank_statement_file_name);
-    let filtered_data = data.filter(
+    let filtered_data = new_data.filter(
         col("Money In")
             .is_not_null()
             .or(col("Money Out").is_not_null().or(col("Fee").is_not_null())),
