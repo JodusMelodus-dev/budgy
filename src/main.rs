1d696cc57490ca8d40067d0612fa07c604ff2548
Philip <philipgricius@gmail.com>
2026-07-05 15:23:42 +0200

    use masks to identify new category

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,15 @@ use std::{
 use polars::{
     df,
     error::PolarsResult,
-    io::{SerWriter, csv::write::CsvWriter},
+    io::{
+        SerReader, SerWriter,
+        csv::{read::CsvReader, write::CsvWriter},
+    },
     lazy::{
-        dsl::{col, lit, when},
+        dsl::{Expr, col, lit, when},
         frame::{IntoLazy, LazyCsvReader, LazyFileListReader},
     },
+    prelude::NULL,
 };
 
 fn read_line(prompt: &str) -> String {
@@ -43,7 +47,20 @@ fn main() -> PolarsResult<()> {
     println!("Opening user profile ...");
     let lookup = LazyCsvReader::new(format!("{}_lookup.csv", username))
         .with_has_header(true)
-        .finish()?;
+        .finish()?
+        .collect()?;
+
+    let masks = lookup.column("Mask")?.str()?;
+    let categories = lookup.column("New Category")?.str()?;
+    let mut expr = lit(NULL);
+
+    for (opt_mask, opt_category) in masks.into_iter().zip(categories.into_iter()) {
+        if let (Some(mask), Some(category)) = (opt_mask, opt_category) {
+            expr = when(col("Description").str().contains(lit(mask), true))
+                .then(lit(category))
+                .otherwise(expr);
+        }
+    }
 
     println!("Opening {} ...", bank_statement_file_name);
     let data = LazyCsvReader::new(bank_statement_path)
@@ -57,10 +74,14 @@ fn main() -> PolarsResult<()> {
             .or(col("Money Out").is_not_null().or(col("Fee").is_not_null())),
     );
 
-    println!("Joining {} and user profile ...", bank_statement_file_name);
     let joined = filtered_data
-        .left_join(lookup, col("Parent Category"), col("Original Category"))
-        .select([col("Nr"), col("Parent Category"), col("New Category")]);
+        .with_column(expr.alias("New Category"))
+        .select([
+            col("Nr"),
+            col("Description"),
+            col("Parent Category"),
+            col("New Category"),
+        ]);
 
     let df = joined.clone().collect()?;
     println!("{}", df);
