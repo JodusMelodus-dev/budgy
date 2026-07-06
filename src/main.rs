4f74370bf52124dcde2e1ce56b1aaae6a9add4e4
Philip <philipgricius@gmail.com>
2026-07-06 19:41:56 +0200

    refactor

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,10 @@ use polars::{
     chunked_array::ops::SortMultipleOptions,
     datatypes::DataType,
     error::PolarsResult,
+    frame::DataFrame,
     lazy::{
-        dsl::{col, dtype_col, lit, when},
-        frame::{LazyCsvReader, LazyFileListReader},
+        dsl::{Expr, col, dtype_col, lit, when},
+        frame::{LazyCsvReader, LazyFileListReader, LazyFrame},
     },
     prelude::NULL,
 };
@@ -25,24 +26,8 @@ fn read_line(prompt: &str) -> String {
     input.trim().to_string()
 }
 
-fn main() -> PolarsResult<()> {
-    unsafe {
-        set_var("POLARS_FMT_MAX_ROWS", "-1");
-        set_var("POLARS_FMT_MAX_COLS", "-1");
-        set_var("POLARS_FMT_STR_LEN", "100");
-    };
-
-    let username = read_line("Enter your username> ");
-    let statement_path = read_line("Enter the path to your bank statement: ");
-    let bank_statement_path = Path::new(&statement_path);
-    let bank_statement_file_name = bank_statement_path
-        .file_name()
-        .expect("Invalid path")
-        .to_str()
-        .expect("Failed to extract file name");
-
-    println!("Opening user profile ...");
-    let mut lookup = LazyCsvReader::new(format!("{}_lookup.csv", username))
+fn load_lookup(path: &Path) -> PolarsResult<LazyFrame> {
+    let mut lookup = LazyCsvReader::new(path)
         .with_has_header(true)
         .finish()?
         .with_columns([dtype_col(&DataType::String).str().strip_chars(lit(""))]);
@@ -55,24 +40,11 @@ fn main() -> PolarsResult<()> {
         .iter()
         .map(|s| s.trim().to_string())
         .collect::<Vec<String>>();
-    let new_lookup = lookup.rename(&old_names, &new_names, false).collect()?;
+    Ok(lookup.rename(&old_names, &new_names, false))
+}
 
-    let masks = new_lookup.column("Mask")?.str()?;
-    let categories = new_lookup.column("New Category")?.str()?;
-    let mut expr = lit(NULL);
-
-    for (opt_mask, opt_category) in masks.into_iter().zip(categories.into_iter()) {
-        if let (Some(mask), Some(category)) = (opt_mask, opt_category) {
-            expr = when(col("Description").str().contains(lit(mask), true))
-                .then(lit(category))
-                .otherwise(expr);
-        }
-    }
-
-    println!("Opening {} ...", bank_statement_file_name);
-    let mut data = LazyCsvReader::new(bank_statement_path)
-        .with_has_header(true)
-        .finish()?;
+fn load_statement(path: &Path) -> PolarsResult<LazyFrame> {
+    let mut data = LazyCsvReader::new(path).with_has_header(true).finish()?;
     let schema = data.collect_schema()?;
     let old_names = schema
         .iter_names()
@@ -84,16 +56,31 @@ fn main() -> PolarsResult<()> {
         .collect::<Vec<String>>();
     let new_data = data.rename(&old_names, &new_names, false);
 
-    println!("Filtering {} ...", bank_statement_file_name);
     let filtered_data = new_data.filter(
         col("Money In")
             .is_not_null()
             .or(col("Money Out").is_not_null().or(col("Fee").is_not_null())),
     );
+    Ok(filtered_data)
+}
 
-    let joined = filtered_data.with_columns([expr.alias("New Category")]);
+fn generate_masks(lookup: DataFrame) -> PolarsResult<Expr> {
+    let masks = lookup.column("Mask")?.str()?;
+    let categories = lookup.column("New Category")?.str()?;
+    let mut expr = lit(NULL);
 
-    let summary = joined
+    for (opt_mask, opt_category) in masks.into_iter().zip(categories.into_iter()) {
+        if let (Some(mask), Some(category)) = (opt_mask, opt_category) {
+            expr = when(col("Description").str().contains(lit(mask), true))
+                .then(lit(category))
+                .otherwise(expr);
+        }
+    }
+    Ok(expr)
+}
+
+fn generate_summary(statement: LazyFrame) -> PolarsResult<()> {
+    let summary = statement
         .clone()
         .filter(col("New Category").is_not_null())
         .group_by([col("New Category")])
@@ -112,9 +99,12 @@ fn main() -> PolarsResult<()> {
 
     let df = summary.clone().collect()?;
     println!("{}", df);
+    Ok(())
+}
 
+fn generate_undefined_categories(statement: LazyFrame) -> PolarsResult<()> {
     println!("===== UNDEFINED CATEGORIES =====");
-    let undefined_categories = joined
+    let undefined_categories = statement
         .filter(col("New Category").is_null())
         .select([col("Description").unique()])
         .sort(
@@ -123,6 +113,28 @@ fn main() -> PolarsResult<()> {
         )
         .collect()?;
     println!("{}", undefined_categories);
+    Ok(())
+}
+
+fn main() -> PolarsResult<()> {
+    unsafe {
+        set_var("POLARS_FMT_MAX_ROWS", "-1");
+        set_var("POLARS_FMT_MAX_COLS", "-1");
+        set_var("POLARS_FMT_STR_LEN", "100");
+    };
+
+    let username = read_line("Enter your username> ");
+    let statement_path = read_line("Enter the path to your bank statement: ");
+    let bank_statement_path = Path::new(&statement_path);
+
+    let lookup = load_lookup(Path::new(&format!("{}_lookup.csv", username)))?.collect()?;
+    let mask_expression = generate_masks(lookup)?;
+
+    let statement =
+        load_statement(bank_statement_path)?.with_columns([mask_expression.alias("New Category")]);
+
+    generate_summary(statement.clone())?;
+    generate_undefined_categories(statement)?;
 
     Ok(())
 }
