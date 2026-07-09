3e0ead9606053a47d086ed48c9fe96d744eef546
Philip <philipgricius@gmail.com>
2026-07-09 15:44:26 +0200

    fix budget tab

--- src/app/tabs/budget.rs
+++ src/app/tabs/budget.rs
@@ -1,13 +1,13 @@
-use crate::app::Budgy;
+use crate::app::{Budgy, help::load_budget};
 
 impl Budgy {
     pub fn display_budget_tab(&mut self, ui: &mut egui::Ui) {
-        if let Some(budget) = self.budget_lf.clone() {
-            self.budget_df = Some(budget.collect().expect("Failed to collect budget"));
-
-            if let Some(budget) = &self.budget_df {
-                self.display_dataframe(ui, &budget);
-            }
+        if let Some(df) = &self.budget_df {
+            self.display_dataframe(ui, df);
+        } else if let Some(lf) = self.budget_lf.clone() {
+            self.budget_df = Some(lf.collect().expect("Failed to collect budget lazy frame"));
+        } else {
+            self.budget_lf = load_budget();
         }
     }
 }
