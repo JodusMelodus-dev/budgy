8755a9a54aa98aa7020ba47517e5f78e8f9ef8a6
Philip <philipgricius@gmail.com>
2026-07-16 13:53:09 +0200

    starting balances

--- src/app/tabs/budget_summary.rs
+++ src/app/tabs/budget_summary.rs
@@ -1,6 +1,10 @@
 use polars::{
     chunked_array::ops::SortMultipleOptions,
-    lazy::dsl::{col, lit},
+    functions::concat_df_diagonal,
+    lazy::{
+        dsl::{col, lit, when},
+        frame::{IntoLazy, LazyFrame},
+    },
 };
 
 use crate::app::Budgy;
@@ -16,6 +20,8 @@ impl Budgy {
         } else {
             if let Some(mut statement) = self.statement_lf.clone() {
                 if let Some(budget) = self.budget_lf.clone() {
+                    statement = Self::add_starting_budget(statement.clone(), budget.clone());
+
                     statement = statement
                         .left_join(budget, col("Category"), col("Category"))
                         .filter(
@@ -69,5 +75,61 @@ impl Budgy {
                 ui.heading("Missing budget");
             }
         }
+    }
+
+    fn add_starting_budget(statement: LazyFrame, budget: LazyFrame) -> LazyFrame {
+        let first_transaction = statement.clone().limit(1).drop([
+            "Nr",
+            "Category",
+            "Transaction Date",
+            "Description",
+            "Original Description",
+            "Parent Category",
+        ]);
+        let starting_balance = first_transaction.with_column(
+            (col("Balance")
+                - when(col("Money In").is_not_null())
+                    .then(col("Money In"))
+                    .when(col("Money Out").is_not_null())
+                    .then(col("Money Out"))
+                    .when(col("Fee").is_not_null())
+                    .then(col("Fee"))
+                    .otherwise(lit(0.0)))
+            .alias("Balance"),
+        );
+
+        let starting_balance_with_budget = starting_balance
+            .cross_join(budget, None)
+            .filter(
+                col("Posting Date")
+                    .lt_eq(col("End Date"))
+                    .and(col("Posting Date").gt_eq(col("Start Date"))),
+            )
+            .drop([
+                "B_Nr",
+                "Color",
+                "Month Difference",
+                "Money In",
+                "Money Out",
+                "Fee",
+                "Start Date",
+                "End Date",
+            ]);
+
+        let ratios = starting_balance_with_budget
+            .with_column((-col("Budget Amount") / col("Budget Amount").max()).alias("Ratio"))
+            .filter(col("Budget Amount").neq(col("Budget Amount").max()));
+
+        let starting = ratios
+            .with_column((col("Balance") * col("Ratio")).alias("Money In"))
+            .drop(["Balance", "Budget Amount", "Ratio"]);
+
+        let statement =
+            concat_df_diagonal(&[statement.collect().unwrap(), starting.collect().unwrap()])
+                .unwrap()
+                .lazy()
+                .drop(["Balance"]);
+
+        statement
     }
 }
