1d86cd0a3e5d8e17f924d30f20ca6b74e526721d
Philip <philipgricius@gmail.com>
2026-07-22 10:57:03 +0200

    Extend scroll Fixes #2

--- src/app/tabs/statement.rs
+++ src/app/tabs/statement.rs
@@ -28,89 +28,100 @@ impl Budgy {
             let height = df.height();
             let column_names = df.get_column_names();
 
-            egui::ScrollArea::horizontal().show(ui, |ui| {
-                egui_extras::TableBuilder::new(ui)
-                    .striped(true)
-                    .resizable(true)
-                    .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
-                    .columns(egui_extras::Column::auto(), column_names.len())
-                    .header(20.0, |mut header| {
-                        for name in &column_names {
-                            header.col(|ui| {
-                                ui.strong(name.to_string());
-                            });
-                        }
-                    })
-                    .body(|body| {
-                        body.rows(22.0, height, |mut row| {
-                            let row_idx = row.index();
-
-                            if let AnyValue::Int64(nr) =
-                                df.column(column_names[0]).unwrap().get(row_idx).unwrap()
-                            {
-                                for column_name in &column_names {
-                                    row.col(|ui| {
-                                        if let Ok(column) = df.column(column_name) {
-                                            let value =
-                                                column.get(row_idx).unwrap_or(AnyValue::Null);
-
-                                            if *column_name == "Category" {
-                                                let current_value = self
-                                                    .statement_deltas
-                                                    .get(&nr)
-                                                    .cloned()
-                                                    .unwrap_or_else(|| {
-                                                        if let AnyValue::String(v) = value {
-                                                            v.to_string()
-                                                        } else {
-                                                            "unknown".to_string()
-                                                        }
-                                                    });
-
-                                                let color = self
-                                                    .config
-                                                    .categories
-                                                    .iter()
-                                                    .find(|(k, _)| *k == current_value)
-                                                    .unwrap_or(&(
-                                                        "".to_string(),
-                                                        Color32::DARK_GRAY,
-                                                    ))
-                                                    .1;
-
-                                                let text =
-                                                    RichText::new(current_value).color(color);
-
-                                                let width = ui.available_width()
-                                                    - ui.spacing().item_spacing.x;
-                                                egui::ComboBox::from_label("")
-                                                    .width(width)
-                                                    .selected_text(text.clone())
-                                                    .show_ui(ui, |ui| {
-                                                        for (category, color) in
-                                                            &self.config.categories
-                                                        {
-                                                            let text = RichText::new(category)
-                                                                .color(*color);
-
-                                                            if ui.button(text).clicked() {
-                                                                self.statement_deltas.insert(
-                                                                    nr,
-                                                                    category.to_string(),
-                                                                );
-                                                            }
-                                                        }
-                                                    });
-                                            } else {
-                                                ui.label(format!("{}", value));
-                                            }
-                                        }
+            egui::ScrollArea::both()
+                .auto_shrink([false, true])
+                .show(ui, |ui| {
+                    ui.vertical(|ui| {
+                        egui_extras::TableBuilder::new(ui)
+                            .striped(true)
+                            .vscroll(false)
+                            .max_scroll_height(f32::INFINITY)
+                            .resizable(true)
+                            .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
+                            .columns(egui_extras::Column::auto(), column_names.len())
+                            .header(20.0, |mut header| {
+                                for name in &column_names {
+                                    header.col(|ui| {
+                                        ui.strong(name.to_string());
                                     });
                                 }
-                            }
-                        });
+                            })
+                            .body(|body| {
+                                body.rows(22.0, height, |mut row| {
+                                    let row_idx = row.index();
+
+                                    if let AnyValue::Int64(nr) =
+                                        df.column(column_names[0]).unwrap().get(row_idx).unwrap()
+                                    {
+                                        for column_name in &column_names {
+                                            row.col(|ui| {
+                                                if let Ok(column) = df.column(column_name) {
+                                                    let value = column
+                                                        .get(row_idx)
+                                                        .unwrap_or(AnyValue::Null);
+
+                                                    if *column_name == "Category" {
+                                                        let current_value = self
+                                                            .statement_deltas
+                                                            .get(&nr)
+                                                            .cloned()
+                                                            .unwrap_or_else(|| {
+                                                                if let AnyValue::String(v) = value {
+                                                                    v.to_string()
+                                                                } else {
+                                                                    "unknown".to_string()
+                                                                }
+                                                            });
+
+                                                        let color = self
+                                                            .config
+                                                            .categories
+                                                            .iter()
+                                                            .find(|(k, _)| *k == current_value)
+                                                            .unwrap_or(&(
+                                                                "".to_string(),
+                                                                Color32::DARK_GRAY,
+                                                            ))
+                                                            .1;
+
+                                                        let text = RichText::new(current_value)
+                                                            .color(color);
+
+                                                        let width = ui.available_width()
+                                                            - ui.spacing().item_spacing.x;
+                                                        egui::ComboBox::from_label("")
+                                                            .width(width)
+                                                            .selected_text(text.clone())
+                                                            .show_ui(ui, |ui| {
+                                                                for (category, color) in
+                                                                    &self.config.categories
+                                                                {
+                                                                    let text =
+                                                                        RichText::new(category)
+                                                                            .color(*color);
+
+                                                                    if ui.button(text).clicked() {
+                                                                        self.statement_deltas
+                                                                            .insert(
+                                                                                nr,
+                                                                                category
+                                                                                    .to_string(),
+                                                                            );
+                                                                    }
+                                                                }
+                                                            });
+                                                    } else {
+                                                        ui.label(format!("{}", value));
+                                                    }
+                                                }
+                                            });
+                                        }
+                                    }
+                                });
+                            });
+                        ui.add_space(3.0 / 4.0 * ui.viewport_rect().height());
                     });
-            });
+                });
         } else if let Some(lf) = self.statement_lf.clone() {
             self.statement_df = Some(
                 lf.collect()
